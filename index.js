62f6f228fd5762914656439fe40885dc4814824f
javier <29498758+javier-hui@users.noreply.github.com>
2022-03-21 16:03:48 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+client.on('ready', (client) => {
+    console.log(`logged in as ${client.user.tag}!`)
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong'
+        })
+    }
+});
+
+client.login(process.env.TOKEN);
